4844563f78ba78143b8ab95886bd3c732ec226b5
thorxenon <pedrolucasrangel1@hotmail.com>
2024-09-15 20:14:41 -0300

    feat: structure layout ui, separing all app tabs in it respective spaces, and responsing it.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+import { Logo } from "@/components/ui/logo";
+import { redirect } from "next/navigation";
 
 export default function Home() {
+
+  redirect('/home');
+
   return (
-   <>
-    ...
-   </>
+   <div className="min-h-screen flex justify-center items-center">
+      <Logo size={80}/>
+   </div>
   );
 }
