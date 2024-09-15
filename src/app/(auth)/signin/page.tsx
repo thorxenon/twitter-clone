43cbc62c9b42b09e4a943b139d4b6db192c26c80
thorxenon <pedrolucasrangel1@hotmail.com>
2024-09-15 10:11:29 -0300

    feat: ui Signin page and it components finished

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,7 +1,22 @@
+import Link from 'next/link';
+import { Logo } from './../../../components/ui/logo';
+import { SignForm } from '@/components/auth/signin-form';
+
 const Signin = () =>{
     return(
-        <div className="max-w-lg flex mx-auto mt-12 px-6">
-            Login
+        <div className="max-w-lg mx-auto mt-12 px-6">
+            <Logo size={56} />
+
+            <h1 className="mt-10 text-2xl">Entre na sua Conta</h1>
+
+            <div className='mt-10 mb-14 flex flex-col gap-6'>
+               <SignForm/>
+            </div>
+
+            <div className="flex flex-col justify-center items-center gap-1 md:flex-row">
+                <div className="text-gray-500">Ainda não tem uma Conta?</div>
+                <Link href="/signup" className='hover:underline'>Cadastre-se</Link>
+            </div>
         </div>
     )
 }
--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -1,7 +1,20 @@
-export const Logo = () =>{
+import Image from "next/image";
+import Link from "next/link";
+
+type logoProps ={
+    size: number;
+}
+
+export const Logo = ({ size }: logoProps) =>{
     return(
-        <>
-        ...
-        </>
+        <Link href="/">
+            <Image
+                src={'/logo.png'}
+                alt="z"
+                width={size}
+                height={size}
+                quality={100}
+            />
+        </Link>
     );
 }
